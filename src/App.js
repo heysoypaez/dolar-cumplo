85b79c2e1cd277c4eb0f3c2927814a4214040226
Daniel Páez <danielpaezsw66@gmail.com>
2019-06-21 18:47:22 -0400

    Validating connection to API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  (async function() {
+  
+    try {
+
+      const PREFIX_API = "https://api.sbif.cl/api-sbifv3/recursos_api/"
+
+      const API_KEY = "9c84db4d447c80c74961a72245371245cb7ac15f"
+
+      const data = await fetch(`${PREFIX_API}dolar?apikey=${API_KEY}&formato=json`)
+      const response = await data.json()
+
+
+      console.log(response.Dolares[0])
+    }
+    catch(error) {
+      console.log("API error:",error)
+    }
+
+    })();
+
   return (
     <div className="App">
       <header className="App-header">
